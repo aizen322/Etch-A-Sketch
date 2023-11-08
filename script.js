7db82101bb3214482ba6f84249389e628b95f5aa
aizen322 <147636587+aizen322@users.noreply.github.com>
2023-11-08 23:38:03 +0800

    added a pen color function which the user can change the color of th pen, added clear button function which will clear the sketchpad(make the sketchpad all white)

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const grid = document.querySelectorAll('div.grid');
 let isMouseDown = false;
+let penColor = document.getElementById('colorPicker');
 
 grid.forEach((pixel) =>{
     pixel.addEventListener('mousedown',()=>{
-        pixel.style.backgroundColor = "black";
+        pixel.style.backgroundColor = penColor.value;
         isMouseDown = true;
     });
     pixel.addEventListener('mouseup',()=>{
-        pixel.style.backgroundColor = "black";
+        pixel.style.backgroundColor = penColor.value;
         isMouseDown = false;
     });
     pixel.addEventListener('mousemove', ()=>{
         if(isMouseDown)
-        pixel.style.backgroundColor = "black";
+        pixel.style.backgroundColor = penColor.value;
     });
 });
+
+/** **************************************************************/
+/**** CLEAR FUNCTION BUTTON */
+const clearbtn = document.getElementById("clear");
+clearbtn.addEventListener('click',()=>{
+    grid.forEach((pixel)=>{
+        pixel.style.backgroundColor = "white";
+    })
+});
+/** **************************************************************/
