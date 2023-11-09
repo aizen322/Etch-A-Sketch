8455f951171d3cfbaaaf6249a65ea9a67d44094d
aizen322 <147636587+aizen322@users.noreply.github.com>
2023-11-09 23:36:25 +0800

    deleted all the div grid in the html code and instead created a function that will generated div grid, created a function which the user can change how many grid can be generated using the slide bar, ***** the pen function is not working, will fix this tomorrow

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+
+let sketchPadSize = 16;
+
+
+function setSize(size){
+    sketchPadSize = size;
+}
+
+
+window.onload = () =>{
+    generateGrid(sketchPadSize);
+}
+
+/** **************************************************************/
+/**** SKETCHPAD RESIZING FUNCTION */
+
+const sketchpad = document.getElementById('sketchpad');
+
+const slider = document.getElementById('slider');
+const sliderValue = document.getElementById('slider-value');
+
+slider.onmousemove = (e) => updateSliderValue(e.target.value);
+slider.onchange = (e) => changeSize(e.target.value);
+
+function updateSliderValue(value){
+    sliderValue.innerHTML = `${value}`;
+}
+
+function changeSize(gridSize){
+    updateSliderValue(gridSize);
+    sketchpad.innerHTML ="";
+
+    setSize(gridSize);
+    generateGrid(gridSize);
+}
+
+
+function generateGrid(gridSize){
+    sketchpad.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    sketchpad.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+
+    for(let i = 0; i<gridSize * gridSize; i++){
+        const gridPixel = document.createElement('div');
+        gridPixel.classList.add('grid');
+        sketchpad.appendChild(gridPixel);
+    }
+}
+/** **************************************************************/
+/**** PEN COLORING FUNCTION */
+
 const grid = document.querySelectorAll('div.grid');
 let isMouseDown = false;
 let penColor = document.getElementById('colorPicker');
@@ -25,4 +75,4 @@ clearbtn.addEventListener('click',()=>{
         pixel.style.backgroundColor = "white";
     })
 });
-/** **************************************************************/
+
